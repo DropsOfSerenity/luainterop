108ac14d5dd1739c327f3c7ca184360a558a0148
Justin Schiff <AmbientRevolution@gmail.com>
2016-05-16 23:49:28 -0700

    Calling a lua function from C

--- main.c
+++ main.c
@@ -22,16 +22,14 @@ main()
   luaL_openlibs(L);
 
   if ( luaL_loadfile(L, "test.lua") )
-  {
     error(L, "failed to loadfile");
-  }
 
-  printf("Calling test.lua from C file...\n");
+  if(lua_pcall(L, 0, 0, 0)) // priming must happen before you can pcall a global
+    error(L, "lua_pcall() failed");
+  lua_getglobal(L, "saysomething");
 
   if(lua_pcall(L, 0, 0, 0))
-  {
-    error(L, "failed to call lua file");
-  }
+    error(L, "lua_pcall() failed");
 
   printf("Successfully called lua from C! :)\n");
 
--- test.lua
+++ test.lua
@@ -1,1 +1,5 @@
 io.write("\nYolo, from Lua\n\n")
+
+function saysomething()
+  io.write("Yolo, from inside .saysomething in lua\n\n")
+end
